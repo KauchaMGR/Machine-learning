1470ad7b68a94db0414cc36e661a5a669787de02
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-03 15:31:18 +0545

    mode of given data

--- mean-mode-median.py
+++ mean-mode-median.py
@@ -1,9 +1,17 @@
-import numpy
+# import numpy
+
+# speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# x = numpy.mean(speed)
+# y= numpy.median(speed)
+
+# print("the mean speed is",x)
+# print("the median speed is",y)
+
+from scipy import stats
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
-x = numpy.mean(speed)
-y= numpy.median(speed)
+x = stats.mode(speed)
 
-print("the mean speed is",x)
-print("the median speed is",y)
+print(x)
