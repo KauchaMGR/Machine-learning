3eefdee96a6731ff5cba95dfc97dae1719505ea7
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-03 15:37:29 +0545

    percentile in python

--- standard-deviation.py
+++ standard-deviation.py
@@ -5,3 +5,9 @@ speed = [86,87,88,86,87,85,86]
 x = numpy.std(speed)
 
 print("standard deviation is",x)
+
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+
+y= numpy.percentile(ages, 75)
+
+print(x)
