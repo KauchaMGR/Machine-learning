8f01fcabffe086650ac21f30709f209ba4cc0d7c
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-05 20:40:20 +0545

    changing the speeed data

--- standard-deviation.py
+++ standard-deviation.py
@@ -6,7 +6,7 @@ x = numpy.std(seed)
 
 print("standard deviation of given data ",x)
 
-ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,51,33]
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,45,32,2,8,6,25,36,27,51,33]
 
 y= numpy.percentile(ages, 75)
 
