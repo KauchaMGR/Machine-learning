e571f19cf2f7a618cbeb84168a2421f4f9e70953
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-05 20:39:44 +0545

    changing the output

--- standard-deviation.py
+++ standard-deviation.py
@@ -4,7 +4,7 @@ seed = [86,87,88,86,87,83,56]
 
 x = numpy.std(seed)
 
-print("standard deviation is",x)
+print("standard deviation of given data ",x)
 
 ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,51,33]
 
