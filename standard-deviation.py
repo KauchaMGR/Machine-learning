63ec56f2e51532dce5a33925c841e4d7f5ba1933
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-06 21:51:29 +0545

    changing the percentile ratio

--- standard-deviation.py
+++ standard-deviation.py
@@ -8,6 +8,6 @@ print("standard deviation of given data ",x)
 
 ages = [5,31,43,48,50,41,7,11,15,39,80,82,45,32,2,8,6,25,36,27,51,33]
 
-y= numpy.percentile(ages, 75)
+y= numpy.percentile(ages, 45)
 
 print("THe percentile of given  age",x)
