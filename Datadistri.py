83702c1b7a71874fd2a29350c395247089de7b39
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-07 23:43:43 +0545

    changing the ratio of sampling of data

--- Datadistri.py
+++ Datadistri.py
@@ -1,8 +1,8 @@
 import numpy 
-x= numpy.random.uniform(0.0,0.5,250)#creating random 250  float values betn 0 to 0.5
+x= numpy.random.uniform(0.0,0.5,20000)#creating random 250  float values betn 0 to 0.5
 print(x)
 
 #for visualizing the data 
 import matplotlib.pyplot as plt
-plt.hist(x,5)
+plt.hist(x,100)
 plt.show()
