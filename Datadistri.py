7e45eee11a74576503d0b1bc4c9bf19593f5a630
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-07 23:47:45 +0545

    normal data distribution

--- Datadistri.py
+++ Datadistri.py
@@ -1,8 +1,17 @@
-import numpy 
-x= numpy.random.uniform(0.0,0.5,20000)#creating random 250  float values betn 0 to 0.5
-print(x)
+# import numpy 
+# x= numpy.random.uniform(0.0,0.5,20000)#creating random 250  float values betn 0 to 0.5
+# print(x)
 
-#for visualizing the data 
+# #for visualizing the data 
+# import matplotlib.pyplot as plt
+# plt.hist(x,100)
+# plt.show()
+
+# for normal data distribution
+import numpy
 import matplotlib.pyplot as plt
-plt.hist(x,100)
+
+x = numpy.random.normal(5.0, 1.0, 100000)
+
+plt.hist(x, 100)
 plt.show()
