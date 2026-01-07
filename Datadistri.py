c1a0c79e4f195fb9f7f099b2d5d214c8e728c53c
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-08 00:00:14 +0545

    line  regession

--- Datadistri.py
+++ Datadistri.py
@@ -1,28 +1,45 @@
-# import numpy 
-# x= numpy.random.uniform(0.0,0.5,20000)#creating random 250  float values betn 0 to 0.5
-# print(x)
+# # import numpy 
+# # x= numpy.random.uniform(0.0,0.5,20000)#creating random 250  float values betn 0 to 0.5
+# # print(x)
 
-# #for visualizing the data 
+# # #for visualizing the data 
+# # import matplotlib.pyplot as plt
+# # plt.hist(x,100)
+# # plt.show()
+
+# # for normal data distribution
+# import numpy
 # import matplotlib.pyplot as plt
-# plt.hist(x,100)
+
+# x = numpy.random.normal(5.0, 1.0, 100000)
+
+# plt.hist(x, 100)
 # plt.show()
 
-# for normal data distribution
-import numpy
+
+# #Sactter plot using scattter() function
+# import numpy
+# import matplotlib.pyplot as plt
+
+# x = numpy.random.normal(5.0, 1.0, 100000)
+# y = numpy.random.normal(5.0, 1.0, 100000)
+
+# plt.scatter(x,y)
+# plt.show()
+
 import matplotlib.pyplot as plt
+from scipy import stats
 
-x = numpy.random.normal(5.0, 1.0, 100000)
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
-plt.hist(x, 100)
-plt.show()
+slope, intercept, r, p, std_err = stats.linregress(x, y)
 
+def myfunc(x):
+  return slope * x + intercept
 
-#Sactter plot using scattter() function
-import numpy
-import matplotlib.pyplot as plt
+mymodel = list(map(myfunc, x))
 
-x = numpy.random.normal(5.0, 1.0, 100000)
-y = numpy.random.normal(5.0, 1.0, 100000)
-
-plt.scatter(x,y)
+plt.scatter(x, y)
+plt.plot(x, mymodel)
 plt.show()
