f5bbfce33601714af0fec5e21ccd55d844cf5b13
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-09 09:10:07 +0545

    wriring the comments

--- scaling.py
+++ scaling.py
@@ -15,19 +15,20 @@ print(scaledX)
 import pandas
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
-scale = StandardScaler()
+scale = StandardScaler()#Creating a scaling object
 
-df = pandas.read_csv("data.csv")
+df = pandas.read_csv("data.csv")#reading data.csv
 
-X = df[['Weight', 'Volume']]
-y = df['CO2']
+X = df[['Weight', 'Volume']]#extracting the column weight and volume
+y = df['CO2']#Extracting the co2 column
 
-scaledX = scale.fit_transform(X)
+scaledX = scale.fit_transform(X)#calculating the mean and std and scaling to store in scaledx object
 
-regr = linear_model.LinearRegression()
-regr.fit(scaledX, y)
+regr = linear_model.LinearRegression()#creating a empty learning model
+regr.fit(scaledX, y)#giving two data inputs so it can find or learn relationship
 
-scaled = scale.transform([[2300, 1.3]])
 
-predictedCO2 = regr.predict([scaled[0]])
-print(predictedCO2)
+scaled = scale.transform([[2300, 1.3]])#providing the new data for calculation of co2
+
+predictedCO2 = regr.predict([scaled[0]])#2D array input requires and selects the first value
+print(predictedCO2)#Gives output
