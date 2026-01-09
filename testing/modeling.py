46872c0aa8c593b4ba75ce6d1cdc76beb5d4bf88
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-09 19:06:06 +0545

    plotting test data

--- testing/modeling.py
+++ testing/modeling.py
@@ -7,5 +7,9 @@ x=np.random.normal(3,1,100)
 y=np.random.normal(150,40,100)/x
 train_x=x[:80]
 train_y=y[:80]
+
+test_x=x[80:]
+test_y=y[80:]
+plt.scatter(test_x,test_y)
 plt.scatter(train_x,train_y)
 plt.show()
