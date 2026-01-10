85cc2cc27c6ab3878ba4f50de0852d181d14140b
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-10 09:21:55 +0545

    calculating the metrics

--- confusionmatrix.py
+++ confusionmatrix.py
@@ -4,9 +4,17 @@ import matplotlib.pyplot as plt
 
 actual=np.random.binomial(1,0.9,1000)
 predicted=np.random.binomial(1,0.9,1000)
-confusion_matrix=metrics.confusion_matrix(actual,predicted)
+confusion_matrix=metrics.confusion_matrix(actual,predicted)#It is a table that is used in classification problems to assess where errors in the model were made.
 cm_display=metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,display_labels=[0,1])
 
 cm_display.plot()
 
 plt.show()
+
+#Now defining the accuracy ,sensitivity,precison,specificty,f_score
+Accuracy = metrics.accuracy_score(actual, predicted)
+Precision = metrics.precision_score(actual, predicted)
+Sensitivity_recall = metrics.recall_score(actual, predicted)
+Specificity = metrics.recall_score(actual, predicted, pos_label=0)
+F1_score = metrics.f1_score(actual, predicted)
+print({"Accuracy":Accuracy,"Precision":Precision,"Sensitivity_recall":Sensitivity_recall,"Specificity":Specificity,"F1_score":F1_score})
