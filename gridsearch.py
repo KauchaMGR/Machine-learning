d102da84f6d34dfd06329d8668d4292f9223981c
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-12 09:45:12 +0545

    default search

---
+++ gridsearch.py
@@ -0,0 +1,17 @@
+#default search i.e C=1
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+logit = LogisticRegression(max_iter = 10000)
+
+print(logit.fit(X,y))
+
+print(logit.score(X,y))
+
+#now using the grid search
+
