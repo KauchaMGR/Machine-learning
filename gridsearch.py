24fde2a407f3ad51b0fd4b18a769f0b4c6c2b820
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-12 09:51:01 +0545

    usimg grid search

--- gridsearch.py
+++ gridsearch.py
@@ -1,4 +1,19 @@
 #default search i.e C=1
+# from sklearn import datasets
+# from sklearn.linear_model import LogisticRegression
+
+# iris = datasets.load_iris()
+
+# X = iris['data']
+# y = iris['target']
+
+# logit = LogisticRegression(max_iter = 10000)
+
+# print(logit.fit(X,y))
+
+# print(logit.score(X,y))
+
+#now using the grid search
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 
@@ -9,9 +24,14 @@ y = iris['target']
 
 logit = LogisticRegression(max_iter = 10000)
 
-print(logit.fit(X,y))
+C = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
 
-print(logit.score(X,y))
+scores = []
 
-#now using the grid search
+for choice in C:
+  logit.set_params(C=choice)
+  logit.fit(X, y)
+  scores.append(logit.score(X, y))
+
+print(scores)#more the value of C greater than the C =1 more the better result
 
