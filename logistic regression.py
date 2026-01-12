c2591f46fa9c76c64ed0c79832f7eb3926b0ec8a
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-12 08:43:41 +0545

    Adding neceesary comment

--- logistic regression.py
+++ logistic regression.py
@@ -32,7 +32,7 @@
 import numpy
 from sklearn import linear_model
 
-X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)#size of tumor in 2 d array with the one columnAdd
 y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
 logr = linear_model.LogisticRegression()
@@ -44,4 +44,4 @@ def logit2prob(logr, X):
   probability = odds / (1 + odds)
   return(probability)
 
-print(logit2prob(logr, X))
+print(logit2prob(logr, X))#at output after 0. explains that in percentage where it is dangerous or not with respect to size of tumer
