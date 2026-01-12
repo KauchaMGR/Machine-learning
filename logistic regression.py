2c7e6bf632c06d6b80ac3f8157a34063723e7c6d
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-12 08:40:32 +0545

    using probability to find wether the tumor is cancerous or not

--- logistic regression.py
+++ logistic regression.py
@@ -13,17 +13,35 @@
 # print(predicted)
 
 #logistic regression  coeffient
+# import numpy
+# from sklearn import linear_model
+
+# #Reshaped for Logistic function.
+# X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+# y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+# logr = linear_model.LogisticRegression()
+# logr.fit(X,y)
+
+# log_odds = logr.coef_
+# odds = numpy.exp(log_odds)
+
+# print(odds)
+
+#probability 
 import numpy
 from sklearn import linear_model
 
-#Reshaped for Logistic function.
 X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
 y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
 logr = linear_model.LogisticRegression()
 logr.fit(X,y)
 
-log_odds = logr.coef_
-odds = numpy.exp(log_odds)
+def logit2prob(logr, X):
+  log_odds = logr.coef_ * X + logr.intercept_
+  odds = numpy.exp(log_odds)
+  probability = odds / (1 + odds)
+  return(probability)
 
-print(odds)
+print(logit2prob(logr, X))
